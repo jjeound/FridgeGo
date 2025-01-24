33c5faba1b74608173b7c7da22e55f6fb556b502
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-01-24 23:21:25 +0900

    Dimens

---
+++ app/src/main/java/com/example/untitled_capstone/core/util/Dimens.kt
@@ -0,0 +1,9 @@
+package com.example.untitled_capstone.core.util
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val surfacePadding = 20.dp
+    val onSurfacePadding = 10.dp
+    val cornerRadius = 12.dp
+}

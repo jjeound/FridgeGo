7f83a0bfaa6a7af901119ff490f5bc4099624cd2
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-03-18 22:56:46 +0900

    constants

--- app/src/main/java/com/example/untitled_capstone/core/util/Constants.kt
+++ app/src/main/java/com/example/untitled_capstone/core/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.untitled_capstone.core.util
 
 object Constants {
     const val NOTIFICATION_CHANNEL_ID = "NOTIFICATION_CHANNEL_ID"
+    const val BASE_URL = "http://refrigerator-env.eba-aatuyi7s.ap-northeast-2.elasticbeanstalk.com/"
 }

39e7e19cb61579f294941ed71386e97b49336327
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-03-18 22:55:46 +0900

    Kakao login

--- app/src/main/java/com/example/untitled_capstone/presentation/feature/login/screen/LoginScreen.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/feature/login/screen/LoginScreen.kt
@@ -1,12 +1,8 @@
-package com.example.untitled_capstone.presentation.feature.login
+package com.example.untitled_capstone.presentation.feature.login.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,21 +11,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.untitled_capstone.R
 import com.example.untitled_capstone.core.util.Dimens
+import com.example.untitled_capstone.presentation.feature.login.KakaoLoginViewModel
 import com.example.untitled_capstone.presentation.feature.login.composable.KakaoLogin
 import com.example.untitled_capstone.ui.theme.CustomTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(navController: NavHostController){
+fun LoginScreen(navController: NavHostController, viewModel: KakaoLoginViewModel){
     Scaffold(
         containerColor = CustomTheme.colors.onSurface,
         topBar = {
@@ -62,7 +56,7 @@ fun LoginScreen(navController: NavHostController){
         Box(
             modifier = Modifier.fillMaxSize().padding(innerPadding),
         ){
-            KakaoLogin()
+            KakaoLogin(viewModel)
         }
     }
 }

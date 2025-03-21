8dacdb39eac030f59eec5820c7b5c50b17b5247b
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-03-21 20:27:23 +0900

    di

--- app/src/main/java/com/example/untitled_capstone/di/AppModule.kt
+++ app/src/main/java/com/example/untitled_capstone/di/AppModule.kt
@@ -7,13 +7,17 @@ import com.example.untitled_capstone.data.local.db.FridgeItemDatabase
 import com.example.untitled_capstone.data.local.remote.FridgeItemDao
 import com.example.untitled_capstone.data.remote.service.Api
 import com.example.untitled_capstone.data.remote.service.LoginApi
+import com.example.untitled_capstone.data.remote.service.MyApi
+import com.example.untitled_capstone.data.remote.service.TokenApi
+import com.example.untitled_capstone.domain.repository.TokenRepository
+import com.example.untitled_capstone.domain.use_case.token.AuthAuthenticator
+import com.example.untitled_capstone.domain.use_case.token.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -21,9 +25,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
 
     @Provides
     @Singleton
@@ -35,9 +36,23 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideApi(): Api {
+    fun provideDao(db: FridgeItemDatabase): FridgeItemDao = db.dao
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(tokenRepository: TokenRepository): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(tokenRepository))
+            .authenticator(AuthAuthenticator(tokenRepository))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(okHttpClient: OkHttpClient): Api {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(Api::class.java)
@@ -45,13 +60,10 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLoginApi(): LoginApi {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
+    fun provideLoginApi(okHttpClient: OkHttpClient): LoginApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(LoginApi::class.java)
@@ -59,6 +71,22 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideDao(db: FridgeItemDatabase): FridgeItemDao = db.dao
+    fun provideMyApi(okHttpClient: OkHttpClient): MyApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MyApi::class.java)
+    }
 
+    @Provides
+    @Singleton
+    fun provideTokenApi(): TokenApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TokenApi::class.java)
+    }
 }

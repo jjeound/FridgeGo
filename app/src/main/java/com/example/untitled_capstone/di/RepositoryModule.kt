f41f207783a614047d0dc3cf0b4a6041bc63020a
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-03-21 20:23:56 +0900

    di

--- app/src/main/java/com/example/untitled_capstone/di/RepositoryModule.kt
+++ app/src/main/java/com/example/untitled_capstone/di/RepositoryModule.kt
@@ -1,12 +1,19 @@
 package com.example.untitled_capstone.di
 
+import android.app.Application
 import com.example.untitled_capstone.data.local.db.FridgeItemDatabase
 import com.example.untitled_capstone.data.remote.service.Api
 import com.example.untitled_capstone.data.remote.service.LoginApi
+import com.example.untitled_capstone.data.remote.service.MyApi
 import com.example.untitled_capstone.data.repository.FridgeRepositoryImpl
+import com.example.untitled_capstone.data.repository.LocalUserMangerImpl
 import com.example.untitled_capstone.data.repository.LoginRepositoryImpl
+import com.example.untitled_capstone.data.repository.MyRepositoryImpl
 import com.example.untitled_capstone.domain.repository.FridgeRepository
+import com.example.untitled_capstone.domain.repository.LocalUserManger
 import com.example.untitled_capstone.domain.repository.LoginRepository
+import com.example.untitled_capstone.domain.repository.MyRepository
+import com.example.untitled_capstone.domain.repository.TokenRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,7 +33,19 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideLoginRepository(api: LoginApi): LoginRepository{
-        return LoginRepositoryImpl(api)
+    fun provideLoginRepository(api: LoginApi, tokenRepository: TokenRepository): LoginRepository{
+        return LoginRepositoryImpl(api, tokenRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMyRepository(api: MyApi, tokenRepository: TokenRepository): MyRepository{
+        return MyRepositoryImpl(api, tokenRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(application: Application): LocalUserManger{
+        return LocalUserMangerImpl(application)
     }
 }
--- app/src/main/java/com/example/untitled_capstone/di/UseCaseModule.kt
+++ app/src/main/java/com/example/untitled_capstone/di/UseCaseModule.kt
@@ -1,7 +1,12 @@
 package com.example.untitled_capstone.di
 
 import com.example.untitled_capstone.domain.repository.FridgeRepository
+import com.example.untitled_capstone.domain.repository.LocalUserManger
 import com.example.untitled_capstone.domain.repository.LoginRepository
+import com.example.untitled_capstone.domain.repository.MyRepository
+import com.example.untitled_capstone.domain.repository.TokenRepository
+import com.example.untitled_capstone.domain.use_case.app_entry.ReadAppEntry
+import com.example.untitled_capstone.domain.use_case.app_entry.SaveAppEntry
 import com.example.untitled_capstone.domain.use_case.fridge.AddFridgeItem
 import com.example.untitled_capstone.domain.use_case.fridge.DeleteFridgeItem
 import com.example.untitled_capstone.domain.use_case.fridge.FridgeUseCases
@@ -9,7 +14,14 @@ import com.example.untitled_capstone.domain.use_case.fridge.GetFridgeItems
 import com.example.untitled_capstone.domain.use_case.fridge.InvalidatePagingSource
 import com.example.untitled_capstone.domain.use_case.fridge.ModifyFridgeItems
 import com.example.untitled_capstone.domain.use_case.fridge.ToggleNotification
-import com.example.untitled_capstone.domain.use_case.kakao.LoginCallback
+import com.example.untitled_capstone.domain.use_case.my.GetAccessToken
+import com.example.untitled_capstone.domain.use_case.my.GetMyProfile
+import com.example.untitled_capstone.domain.use_case.my.GetOtherProfile
+import com.example.untitled_capstone.domain.use_case.login.KakaoLogin
+import com.example.untitled_capstone.domain.use_case.login.LoginUseCases
+import com.example.untitled_capstone.domain.use_case.my.Logout
+import com.example.untitled_capstone.domain.use_case.login.ValidateNickname
+import com.example.untitled_capstone.domain.use_case.my.MyUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,7 +45,35 @@ object UseCaseModule {
     }
 
     @Provides
-    fun provideLoginCallback(repository: LoginRepository): LoginCallback {
-        return LoginCallback(repository)
+    fun provideLoginUseCases(repository: LoginRepository): LoginUseCases {
+        return LoginUseCases(
+            kakaoLogin = KakaoLogin(repository),
+            validateNickname = ValidateNickname(repository)
+        )
+    }
+
+    @Provides
+    fun provideReadAppEntry(localUserManger: LocalUserManger): ReadAppEntry {
+        return ReadAppEntry(localUserManger)
+    }
+
+    @Provides
+    fun provideSaveAppEntry(localUserManger: LocalUserManger): SaveAppEntry {
+        return SaveAppEntry(localUserManger)
+    }
+
+    @Provides
+    fun provideMyUseCases(repository: MyRepository, tokenRepository: TokenRepository): MyUseCases {
+        return MyUseCases(
+            getMyProfile = GetMyProfile(repository),
+            getOtherProfile = GetOtherProfile(repository),
+            logout = Logout(repository),
+            getAccessToken = GetAccessToken(tokenRepository)
+        )
+    }
+
+    @Provides
+    fun provideGetAccessToken(tokenRepository: TokenRepository): GetAccessToken{
+        return GetAccessToken(tokenRepository)
     }
 }

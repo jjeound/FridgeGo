f4c11e668794fadb4741c974c73707b9506ae162
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-01 23:47:53 +0900

    map

---
+++ app/src/main/java/com/example/untitled_capstone/data/remote/dto/DocumentsDto.kt
@@ -0,0 +1,6 @@
+package com.example.untitled_capstone.data.remote.dto
+
+data class DocumentsDto(
+    val road_address: RoadAddressDto,
+    val address: AddressDto,
+)
---
+++ app/src/main/java/com/example/untitled_capstone/data/remote/dto/MetaDto.kt
@@ -0,0 +1,5 @@
+package com.example.untitled_capstone.data.remote.dto
+
+data class MetaDto(
+    val total_count: Int
+)

0960f50401c8c508ad8e558e51642bf4b5311236
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-01 23:47:15 +0900

    post

--- app/src/main/java/com/example/untitled_capstone/domain/use_case/post/GetLikedPosts.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/use_case/post/GetLikedPosts.kt
@@ -2,17 +2,18 @@ package com.example.untitled_capstone.domain.use_case.post
 
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.example.untitled_capstone.data.util.PostFetchType
 import com.example.untitled_capstone.domain.model.PostRaw
 import com.example.untitled_capstone.domain.repository.PostRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class GetPostItems @Inject constructor(
+class GetLikedPosts @Inject constructor(
     private val repository: PostRepository
 ) {
-    operator fun invoke(): Flow<PagingData<PostRaw>> {
-        return repository.getPosts().map { pagingData ->
+    operator fun invoke(fetchType: PostFetchType): Flow<PagingData<PostRaw>> {
+        return repository.getLikedPosts(fetchType).map { pagingData ->
             pagingData.map { it.toPostRaw() }
         }
     }

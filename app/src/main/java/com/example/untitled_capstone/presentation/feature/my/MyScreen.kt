04a5db97470fcb79b24708e2e46deb332075eeda
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-06 21:58:34 +0900

    my

--- app/src/main/java/com/example/untitled_capstone/presentation/feature/my/MyScreen.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/feature/my/MyScreen.kt
@@ -1,5 +1,14 @@
 package com.example.untitled_capstone.presentation.feature.my
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -8,19 +17,45 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
+import com.example.untitled_capstone.MainActivity
 import com.example.untitled_capstone.R
 import com.example.untitled_capstone.core.util.Dimens
 import com.example.untitled_capstone.navigation.Graph
 import com.example.untitled_capstone.navigation.Screen
+import com.example.untitled_capstone.presentation.feature.fridge.composable.PermissionDialog
+import com.example.untitled_capstone.presentation.feature.login.LoginEvent
 import com.example.untitled_capstone.presentation.feature.my.composable.MyAccountContainer
 import com.example.untitled_capstone.presentation.feature.my.composable.MyContainer
 import com.example.untitled_capstone.ui.theme.CustomTheme
 
 @Composable
 fun MyScreen(navController: NavHostController, onEvent: (MyEvent) -> Unit, state: MyState) {
+    val locationPermissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { permissions ->
+                locationPermissions.forEach { permission ->
+                    if (permissions[permission] == true){
+                        Log.d(permission, "위치 권한이 허용되었습니다.")
+                    }
+                }
+            }
+        )
+    val context = LocalContext.current
+    val packageName = context.packageName
+    val showDialog = remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.padding(horizontal = Dimens.surfaceHorizontalPadding,
             vertical = Dimens.surfaceVerticalPadding),
@@ -73,7 +108,16 @@ fun MyScreen(navController: NavHostController, onEvent: (MyEvent) -> Unit, state
                     style = CustomTheme.typography.title2,
                     color = CustomTheme.colors.textPrimary,
                 )
-                MyContainer("내 동네 설정", R.drawable.location, {navController.navigate(Screen.LocationNav)})
+
+                MyContainer("내 동네 설정", R.drawable.location){
+                    checkPermission(locationPermissions, context, showDialog){
+                        if(it){
+                            navController.navigate(Screen.LocationNav)
+                        }else{
+                            requestPermissionLauncher.launch(locationPermissions)
+                        }
+                    }
+                }
                 MyContainer("앱 설정", R.drawable.info, {})
             }
         }
@@ -96,6 +140,43 @@ fun MyScreen(navController: NavHostController, onEvent: (MyEvent) -> Unit, state
                 MyContainer("고객센터", R.drawable.headset, {})
                 MyContainer("약관 및 정책", R.drawable.setting, {})
             }
+        }
+    }
+    PermissionDialog(
+        showDialog = showDialog,
+        message = "위치 권한이 필요합니다.",
+        onDismiss = { showDialog.value = false },
+        onConfirm = {
+            showDialog.value = false
+            context.startActivity(
+                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+            )
+        }
+    )
+}
+
+private fun checkPermission(locationPermissions: Array<String>, context: Context, showDialog: MutableState<Boolean>, onRequest: (Boolean) -> Unit) {
+    when {
+        ContextCompat.checkSelfPermission(
+            context,
+            locationPermissions[0]
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    locationPermissions[1]
+                ) == PackageManager.PERMISSION_GRANTED->  {
+            onRequest(true)
+        }
+        shouldShowRequestPermissionRationale(
+            context as MainActivity,
+            locationPermissions[0]
+        ) -> {
+            showDialog.value = true
+        }
+        else -> {
+            onRequest(false)
         }
     }
 }

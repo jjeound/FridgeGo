9a9abe6c50e18dd0b8f2ca728fd356d6e43d78c2
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-09 14:11:06 +0900

    webSocket

--- app/src/main/java/com/example/untitled_capstone/data/repository/WebSocketRepositoryImpl.kt
+++ app/src/main/java/com/example/untitled_capstone/data/repository/WebSocketRepositoryImpl.kt
@@ -3,12 +3,15 @@ package com.example.untitled_capstone.data.repository
 import com.example.untitled_capstone.data.remote.manager.WebSocketManager
 import com.example.untitled_capstone.domain.model.Message
 import com.example.untitled_capstone.domain.model.UnreadBroadcast
+import com.example.untitled_capstone.domain.repository.TokenRepository
 import com.example.untitled_capstone.domain.repository.WebSocketRepository
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 
 class WebSocketRepositoryImpl @Inject constructor(
-    private val webSocketManager: WebSocketManager
+    private val webSocketManager: WebSocketManager,
 ) : WebSocketRepository {
 
     override fun connect(token: String, roomId: Long, onConnected: () -> Unit, onError: (Throwable) -> Unit) {

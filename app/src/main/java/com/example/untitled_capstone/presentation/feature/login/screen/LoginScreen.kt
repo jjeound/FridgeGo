817f9d72d5d0234ac255c5996616ad7670f09d04
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-12 23:26:15 +0900

    login

--- app/src/main/java/com/example/untitled_capstone/presentation/feature/login/screen/LoginScreen.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/feature/login/screen/LoginScreen.kt
@@ -5,17 +5,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavHostController
-import com.example.untitled_capstone.R
 import com.example.untitled_capstone.core.util.Dimens
 import com.example.untitled_capstone.presentation.feature.login.LoginEvent
 import com.example.untitled_capstone.presentation.feature.login.composable.KakaoLogin

18751d1594801c0cc9fd17f8d323948dc9fbf7de
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-14 21:12:24 +0900

    login

--- app/src/main/java/com/example/untitled_capstone/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/example/untitled_capstone/data/repository/LoginRepositoryImpl.kt
@@ -35,7 +35,7 @@ class LoginRepositoryImpl @Inject constructor(
         return try {
             Resource.Loading(data = null)
             //val response = api.kakaoLogin(accessToken)
-            val response = api.loginTest(EmailReq("2"))
+            val response = api.loginTest(EmailReq("1"))
             if(response.isSuccess){
                 tokenRepository.saveAccessToken(response.result!!.accessToken)
                 tokenRepository.saveRefreshToken(response.result.refreshToken)

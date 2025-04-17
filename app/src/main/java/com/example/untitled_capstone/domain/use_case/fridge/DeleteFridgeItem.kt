0827718bdaa49bba30b22da3537c7c730cf25e6e
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-17 19:43:06 +0900

    fridge

--- app/src/main/java/com/example/untitled_capstone/domain/use_case/fridge/DeleteFridgeItem.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/use_case/fridge/DeleteFridgeItem.kt
@@ -1,15 +1,13 @@
 package com.example.untitled_capstone.domain.use_case.fridge
 
 import com.example.untitled_capstone.core.util.Resource
-import com.example.untitled_capstone.data.remote.dto.ApiResponse
-import com.example.untitled_capstone.domain.model.FridgeItem
 import com.example.untitled_capstone.domain.repository.FridgeRepository
 import javax.inject.Inject
 
 class DeleteFridgeItem @Inject constructor(
     private val fridgeRepository: FridgeRepository
 ) {
-    suspend operator fun invoke(id: Long): Resource<ApiResponse> {
+    suspend operator fun invoke(id: Long): Resource<String> {
         return fridgeRepository.deleteItem(id)
     }
 }

6da5fea26728be36d74ebd7393280cbd2c40e41c
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-17 19:43:20 +0900

    home

--- app/src/main/java/com/example/untitled_capstone/data/remote/dto/ImageDto.kt
+++ app/src/main/java/com/example/untitled_capstone/data/remote/dto/ImageDto.kt
@@ -1,6 +1,6 @@
 package com.example.untitled_capstone.data.remote.dto
 
-import com.example.untitled_capstone.domain.use_case.post.PostImage
+import com.example.untitled_capstone.domain.model.PostImage
 
 data class ImageDto(
     val imageUrl: String,
--- app/src/main/java/com/example/untitled_capstone/data/remote/dto/PreferenceDto.kt
+++ app/src/main/java/com/example/untitled_capstone/data/remote/dto/PreferenceDto.kt
@@ -1,5 +1,13 @@
 package com.example.untitled_capstone.data.remote.dto
 
+import com.example.untitled_capstone.domain.model.TastePreference
+
 data class PreferenceDto(
     val tastePreference: String
-)
+){
+    fun toTastePreference(): TastePreference{
+        return TastePreference(
+            tastePreference = tastePreference
+        )
+    }
+}

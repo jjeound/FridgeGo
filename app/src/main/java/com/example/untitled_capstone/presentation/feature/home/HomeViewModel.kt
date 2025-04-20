9e5b1ec3fa492f64750519445b1e02d18db2bd18
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-04-20 18:41:58 +0900

    home

--- app/src/main/java/com/example/untitled_capstone/presentation/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/feature/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.untitled_capstone.presentation.feature.home
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue

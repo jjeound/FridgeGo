9df8594715501c91fde6ac56f1c92f17415b305e
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-21 23:52:14 +0900

    const

--- app/src/main/java/com/example/untitled_capstone/core/util/PrefKeys.kt
+++ app/src/main/java/com/example/untitled_capstone/core/util/PrefKeys.kt
@@ -1,11 +1,12 @@
 package com.example.untitled_capstone.core.util
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PrefKeys {
     val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
     val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
     val NICKNAME = stringPreferencesKey("nickname")
-    val EMAIL = stringPreferencesKey("email")
-    val IMAGE_URL = stringPreferencesKey("image_url")
+    val APP_ENTRY = booleanPreferencesKey("app_entry")
+    val MY_PROFILE = stringPreferencesKey("my_profile")
 }

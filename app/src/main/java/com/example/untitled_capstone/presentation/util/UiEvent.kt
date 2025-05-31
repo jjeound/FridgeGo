1c4b0cdc780fea049a3c090495976676859af8b2
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-31 22:53:35 +0900

    ui event

--- app/src/main/java/com/example/untitled_capstone/presentation/util/UiEvent.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/util/UiEvent.kt
@@ -1,10 +1,5 @@
 package com.example.untitled_capstone.presentation.util
 
-import com.example.untitled_capstone.navigation.Screen
-
-
 sealed class UiEvent{
     data class ShowSnackbar(val message: String) : UiEvent()
-    data class Navigate(val route: Screen) : UiEvent()
-    object PopBackStack : UiEvent()
 }
--- app/src/main/java/com/example/untitled_capstone/presentation/util/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.untitled_capstone.presentation.util
-
-interface UiState {
-    data object Success : UiState
-    data object Loading : UiState
-    data class Error(val message: String?) : UiState
-}

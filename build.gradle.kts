e2aabeb2821004c21b5dc18e8ea38787400ba819
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-31 22:54:23 +0900

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -7,4 +6,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.dagger.hilt) apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }

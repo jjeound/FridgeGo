30480a5974d127f9077b380f7900c6c46515bfce
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-31 22:53:44 +0900

    main activity

--- app/src/main/java/com/example/untitled_capstone/MainActivity.kt
+++ app/src/main/java/com/example/untitled_capstone/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.untitled_capstone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.runtime.collectAsState
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.untitled_capstone.presentation.feature.main.MainScreen
 import com.example.untitled_capstone.presentation.feature.main.MainViewModel
 import com.example.untitled_capstone.ui.theme.Untitled_CapstoneTheme
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,7 +24,6 @@ class MainActivity : ComponentActivity() {
                 condition = {viewModel.splashCondition.value}
             )
         }
-
         enableEdgeToEdge()
         setContent {
             Untitled_CapstoneTheme {

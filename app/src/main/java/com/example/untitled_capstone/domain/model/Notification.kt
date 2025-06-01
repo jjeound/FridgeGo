9b741aa5e904ea79ea762bee84006e3e62b72366
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-01 22:25:52 +0900

    notification

--- app/src/main/java/com/example/untitled_capstone/domain/model/Notification.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/model/Notification.kt
@@ -1,9 +1,19 @@
 package com.example.untitled_capstone.domain.model
 
+import com.example.untitled_capstone.data.local.entity.NotificationEntity
+
 data class Notification(
     val title: String,
     val content: String,
     val time: String,
     val isRead: Boolean,
-    val navigation: String
-)
+){
+    fun toEntity(): NotificationEntity{
+        return NotificationEntity(
+            title = title,
+            content = content,
+            time = time,
+            isRead = isRead
+        )
+    }
+}

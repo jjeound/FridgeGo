365bded63d66c261aacb7864d43ea8218ae4a779
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-01 22:26:22 +0900

    my

--- app/src/main/java/com/example/untitled_capstone/domain/use_case/my/UploadProfileImageUseCase.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/use_case/my/UploadProfileImageUseCase.kt
@@ -3,9 +3,6 @@ package com.example.untitled_capstone.domain.use_case.my
 import com.example.untitled_capstone.core.util.Resource
 import com.example.untitled_capstone.domain.repository.MyRepository
 import kotlinx.coroutines.flow.Flow
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import javax.inject.Inject
 
@@ -13,8 +10,6 @@ class UploadProfileImageUseCase @Inject constructor(
     private val myRepository: MyRepository
 ) {
     operator fun invoke(file: File): Flow<Resource<String>> {
-        val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
-        val body = MultipartBody.Part.createFormData("profileImage", file.name, requestFile)
-        return myRepository.uploadProfileImage(body)
+        return myRepository.uploadProfileImage(file)
     }
 }

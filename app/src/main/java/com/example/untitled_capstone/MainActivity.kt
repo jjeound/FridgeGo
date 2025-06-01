6f17f55c10ef1a2f98ed407dbc1118bc69567816
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-01 22:27:01 +0900

    main

--- app/src/main/java/com/example/untitled_capstone/MainActivity.kt
+++ app/src/main/java/com/example/untitled_capstone/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.untitled_capstone
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +9,6 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.untitled_capstone.presentation.feature.main.MainScreen
 import com.example.untitled_capstone.presentation.feature.main.MainViewModel
 import com.example.untitled_capstone.ui.theme.Untitled_CapstoneTheme
-import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint

b7fbe9d488db11c02ff84b310972ef02fa51b281
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-01 22:25:37 +0900

    image compressor

--- app/src/main/java/com/example/untitled_capstone/data/util/PostFetchType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.untitled_capstone.data.util
-
-sealed class PostFetchType {
-    object MyPosts : PostFetchType() // 내가 쓴 글 조회
-    object LikedPosts : PostFetchType() // 내가 좋아요한 글 조회
-    data class Search(val keyword: String?) : PostFetchType() // 검색한 글 조회
-}

84d36ee7f55ae0ac73b2a53b2fa2a168a53206c4
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-01 22:26:13 +0900

    fridge

--- app/src/main/java/com/example/untitled_capstone/domain/use_case/fridge/ModifyFridgeItemUseCase.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/use_case/fridge/ModifyFridgeItemUseCase.kt
@@ -4,9 +4,6 @@ import com.example.untitled_capstone.core.util.Resource
 import com.example.untitled_capstone.domain.model.FridgeItem
 import com.example.untitled_capstone.domain.repository.FridgeRepository
 import kotlinx.coroutines.flow.Flow
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import javax.inject.Inject
 
@@ -14,11 +11,6 @@ class ModifyFridgeItemUseCase @Inject constructor(
     private val fridgeRepository: FridgeRepository
 ) {
     operator fun invoke(item: FridgeItem, image: File?): Flow<Resource<String>> {
-        if (image == null) {
-            return fridgeRepository.modifyItem(item)
-        }
-        val requestFile = image.asRequestBody("image/*".toMediaTypeOrNull())
-        val body = MultipartBody.Part.createFormData("ingredientImage", image.name, requestFile)
-        return fridgeRepository.modifyItem(item, body)
+        return fridgeRepository.modifyItem(item, image)
     }
 }

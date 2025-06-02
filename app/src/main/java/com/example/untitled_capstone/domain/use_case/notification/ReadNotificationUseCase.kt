33bbd0e5aa0a560d413452cd2215844481f3856d
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-02 20:01:52 +0900

    notification

--- app/src/main/java/com/example/untitled_capstone/domain/use_case/notification/ReadNotificationUseCase.kt
+++ app/src/main/java/com/example/untitled_capstone/domain/use_case/notification/ReadNotificationUseCase.kt
@@ -1,7 +1,6 @@
 package com.example.untitled_capstone.domain.use_case.notification
 
 import com.example.untitled_capstone.domain.repository.NotificationRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ReadNotificationUseCase @Inject constructor(

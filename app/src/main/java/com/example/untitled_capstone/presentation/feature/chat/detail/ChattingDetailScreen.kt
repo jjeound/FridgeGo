97fd3f800ac3b49d40c1398c1128a80668f6ef64
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-02 20:02:08 +0900

    chat

--- app/src/main/java/com/example/untitled_capstone/presentation/feature/chat/detail/ChattingDetailScreen.kt
+++ app/src/main/java/com/example/untitled_capstone/presentation/feature/chat/detail/ChattingDetailScreen.kt
@@ -20,17 +20,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -39,7 +33,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.navigation.NavHostController
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.example.untitled_capstone.R
@@ -48,9 +41,6 @@ import com.example.untitled_capstone.domain.model.ChatMember
 import com.example.untitled_capstone.domain.model.ChattingRoom
 import com.example.untitled_capstone.domain.model.Message
 import com.example.untitled_capstone.navigation.Screen
-import com.example.untitled_capstone.presentation.feature.chat.ChatViewModel
-import com.example.untitled_capstone.presentation.util.CustomSnackbar
-import com.example.untitled_capstone.presentation.util.UiEvent
 import com.example.untitled_capstone.ui.theme.CustomTheme
 
 @OptIn(ExperimentalMaterial3Api::class)

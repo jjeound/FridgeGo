98a6991e69e2f45bf88903523ca1ed584b43310f
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-02 20:01:32 +0900

    di

--- app/src/main/java/com/example/untitled_capstone/di/AppModule.kt
+++ app/src/main/java/com/example/untitled_capstone/di/AppModule.kt
@@ -60,7 +60,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module

82499e78aec7ef602b1d419a0f17334eca573bd0
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-06-07 20:29:05 +0900

    okhttp timeout 세팅

--- app/src/main/java/com/stone/fridge/di/AppModule.kt
+++ app/src/main/java/com/stone/fridge/di/AppModule.kt
@@ -57,6 +57,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -139,6 +140,9 @@ object AppModule {
     @Singleton
     fun provideOkHttpClient(tokenRepository: TokenRepository): OkHttpClient {
         return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS) // 연결 타임아웃
+            .readTimeout(30, TimeUnit.SECONDS)    // 읽기 타임아웃
+            .writeTimeout(30, TimeUnit.SECONDS)   // 쓰기 타임아웃
             .addInterceptor(AuthInterceptor(tokenRepository))
             .authenticator(AuthAuthenticator(tokenRepository))
             .build()

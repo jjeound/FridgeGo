00cbc1ae8667cf81cab6c388c4ee4ce2f374d68c
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-08-05 17:51:11 +0900

    create ui module[#466]

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("stone.fridge.android.feature")
+}
+
+android {
+    namespace = "com.stone.fridge.core.ui"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}

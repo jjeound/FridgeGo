bdb4898ae7f912cabce8cd520f1a9708514d3ee5
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-08-09 19:52:48 +0900

    [#468] preference keys

--- core/data/src/main/kotlin/com/stone/fridge/core/data/util/PrefKeys.kt
+++ core/data/src/main/kotlin/com/stone/fridge/core/data/util/PrefKeys.kt
@@ -1,12 +1,10 @@
-package com.stone.fridge.core.util
+package com.stone.fridge.core.data.util
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PrefKeys {
-    val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
-    val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
     val NICKNAME = stringPreferencesKey("nickname")
     val USER_ID = longPreferencesKey("user_id")
     val APP_ENTRY = booleanPreferencesKey("app_entry")
@@ -14,4 +12,6 @@ object PrefKeys {
     val IS_UNREAD_NOTIFICATION = booleanPreferencesKey("is_unread_notification")
     val FCM_OLD_TOKEN = stringPreferencesKey("fcm_old_token")
     val FCM_NEW_TOKEN = stringPreferencesKey("fcm_new_token")
+    val IS_FIRST_SELECTION = booleanPreferencesKey("is_first_selection")
+    val TASTE_PREFERENCE = stringPreferencesKey("taste_preference")
 }

3333ea282eeff6d85031d6725825ed1abb5949c7
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-08-09 19:43:54 +0900

    [#468] 기존 app 모듈 삭제

--- app/src/main/kotlin/com/stone/fridge/domain/model/Keyword.kt
+++
@@ -1,5 +0,0 @@
-package com.stone.fridge.domain.model
-
-data class Keyword(
-    val keyword: String
-)
--- app/src/main/kotlin/com/stone/fridge/presentation/util/UiEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.stone.fridge.presentation.util
-
-sealed class UiEvent{
-    data class ShowSnackbar(val message: String) : UiEvent()
-}

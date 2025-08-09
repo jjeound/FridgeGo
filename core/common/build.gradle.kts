08200d330aa93626e856c19dc39a7744b78e9ea2
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-08-09 19:48:56 +0900

    [#468] common 상수, 확장함수

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("stone.fridge.android.library")
+    id("stone.fridge.spotless")
 }
 
 android {
@@ -8,11 +8,5 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ core/common/src/main/kotlin/com/stone/fridge/core/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.stone.fridge.core.common
+
+object Constants {
+    const val NOTIFICATION_CHANNEL_ID = "NOTIFICATION_CHANNEL_ID"
+    const val FCM_CHANNEL_ID = "FCM_CHANNEL_ID"
+}

52e0dcc5a5e586f1715e447cd8836883d8091f2e
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-08-09 19:53:15 +0900

    [#468] data gradle

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,47 +1,30 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("stone.fridge.android.library")
+    id("stone.fridge.android.hilt")
+    id("stone.fridge.spotless")
 }
 
 android {
     namespace = "com.stone.fridge.core.data"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        compilerOptions {
-            freeCompilerArgs.add("-Xcontext-receivers")
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-            freeCompilerArgs.add("-XXLanguage:+PropertyParamAnnotationDefaultTargetMode")
-        }
-    }
 }
 
 dependencies {
+    // core modules
+    api(projects.core.model)
+    api(projects.core.common)
+    implementation(projects.core.network)
+    implementation(projects.core.database)
+    implementation(projects.core.auth)
+    api(projects.core.paging)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation (libs.androidx.paging.common)
+    implementation(libs.retrofit)
+    implementation (libs.androidx.datastore.preferences)
+    implementation (libs.converter.gson)
+    // coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    // unit test
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
